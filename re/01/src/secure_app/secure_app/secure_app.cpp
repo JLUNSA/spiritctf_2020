f2ae6cf8ef9715c2f2264f5581d343afe26a2758
kotori0 <admin@0x.mk>
2020-10-12 17:09:58 +0800

    re01-02 src

---
+++ re/01/src/secure_app/secure_app/secure_app.cpp
@@ -0,0 +1,18 @@
+﻿// secure_app.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+// Spirit{2aa57287-603a-4c2e-b7a8-978dae5fdd1f}
+const char* flag = "}f1ddf5ead879-8a7b-e2c4-a306-78275aa2{tiripS";
+int main()
+{
+    char rev[20];
+    for (int i = 0; i < 20; i++) {
+        rev[19 - i] = flag[24 + i];
+    }
+    rev[19] = 0;
+    printf("For security reasons, I can only give you first part of your flags.\n");
+    printf("It is: %s\n", rev);
+    printf("Good Luck!\n");
+    system("pause");
+}
---
+++ re/02/src/keygen/keygen.cpp
@@ -0,0 +1,62 @@
+﻿// keygen.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <Windows.h>
+#include <iostream>
+
+DWORD WINAPI antidebug1(LPVOID lpParameter) {
+    while (true) {
+        if (IsDebuggerPresent()) {
+            exit(0);
+        }
+    }
+}
+
+BOOL CheckDebug()
+{
+    char result = 0;
+    __asm
+    {
+        mov eax, fs: [30h]
+        mov al, BYTE PTR[eax + 2]
+        mov result, al
+    }
+    return result != 0;
+}
+
+DWORD WINAPI antidebug2(LPVOID lpParameter) {
+    srand(0x1145140);
+    while (true) {
+        if (CheckDebug()) {
+            exit(0);
+        }
+    }
+}
+
+// Spirit{a59298c9-3cde-4684-9784-f456bd324e97}
+const char encrypted_flag[] = "a59298c9-3cde-4684-9784-f456bd324e97"; // patched after compile
+__declspec(noinline) uint8_t* get_flag() {
+    uint8_t* flag = new uint8_t[37];
+    for (int i = 0; i < 36; i++) {
+        uint8_t key = rand() % 0xff;
+        printf("%02x", key);
+        flag[i] = key ^ encrypted_flag[i];
+    }
+    flag[36] = 0;
+    return flag;
+}
+
+int main()
+{
+    HANDLE thread1 = CreateThread(NULL, 0, antidebug1, NULL, 0, NULL);
+    HANDLE thread2 = CreateThread(NULL, 0, antidebug2, NULL, 0, NULL);
+    char buf[0x100];
+    memset(buf, 0, 0x100);
+    printf("Enter your license key to get the flag: ");
+    scanf_s("%s", buf, 0x100);
+    uint8_t* flag = get_flag();
+    if (strcmp(buf, (char*)flag) == 0) {
+        printf("Accepted! Your flag is Spirit{%s}", buf);
+    } else {
+        printf("Please try again!");
+    }
+}
